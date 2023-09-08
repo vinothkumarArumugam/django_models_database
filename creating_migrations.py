1d1d20a7cecee12349a1d35c7a3ef120c97a1aed
vinothkumar arumugam <132250443+vinothkumarArumugam@users.noreply.github.com>
2023-09-08 02:31:48 -0400

    creating_migrations.py

---
+++ creating_migrations.py
@@ -0,0 +1,5 @@
+# enter python manage.py makemigration---- it will tend to crete table(employee) in database 
+#enter python manage.py migrate ------it will create table in db(database)
+#enter python manage.py sqlmigrate appname(your app created ) then numbers in cmd-------if you want to check how the table will looks or created in db
+# enter python manage.py createsuperuser ----to create admin authentication like username password
+
